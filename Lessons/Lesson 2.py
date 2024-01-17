01eee008becf6c790666ff09a50d083d16f22dd0
Braynin-Alexandr <braynin.alexander@gmail.com>
2024-01-18 00:03:09 +0300

    Второе занятие

---
+++ Lessons/Lesson 2.py
@@ -0,0 +1,39 @@
+# for x in (range(1,int(input("Введите число: "))+1)):
+#     if x%15==0:print(f"FizzBuuz", end=" ")
+#     elif x%3==0:print(f"Fizz", end=" ")
+#     elif x%5==0:print(f"Buzz", end=" ")
+#     print(x, end=" ")
+
+#
+# for x in "Hello World":
+#     if x=="o":continue
+#     print(x*2, end="")
+# else:
+#     print("\nagain")
+
+# number=int(input("Число: "))
+#
+# for x in range(number+1):
+#     print(x, end=":")
+#     if x>10:break
+#     elif x%2==0:continue
+#     else: print("         ", x)
+#
+# n=int(input())
+# for x in range(n+1):
+#     print("+"*x)
+
+#
+#
+# n = int(input())
+# for x in range(1, n + 1):
+#     for y in range(x):
+#         print("+", end="")
+#     print()
+
+
+# lst=[]
+# for x in range(5):
+#     a=int(input("Введите число: "))
+#     lst.append(a)
+#     print(lst)
