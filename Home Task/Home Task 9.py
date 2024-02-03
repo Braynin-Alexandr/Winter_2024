3877225c5d79dcceec46aaec7d0d469fb30e6761
Braynin-Alexandr <braynin.alexander@gmail.com>
2024-02-03 06:47:15 +0300

    дз, обновил

---
+++ Home Task/Home Task 9.py
@@ -0,0 +1,58 @@
+# #Задача 9-1. ДНК в РНК
+# def do_rnk(a):
+#
+#     dct={"G":"C", "T":"A","C":"G", "A":"T"}
+#     res=f"РНК: "
+#
+#     for x in s:
+#         if x in dct:
+#             res+=dct.get(x)
+#         else: return "Ошибка ввода"
+#     return res
+#
+# #GGCTAA
+# s=input("Ввод ДНК: ").upper()
+# print(do_rnk(s))
+# print()
+
+#Задача 9-2.Частотный анализ текста
+
+# letters=("а", "у", "о", "ы", "и", "э", "я", "ю", "ё", "е")
+#
+# n=int(input("Кол-во слов: "))
+# s1=input("Первое слово: ").lower()
+# s2=input(f"Введите остальные слова: ").lower().split()
+# dct1={}
+# dct2={}
+#
+#
+# for i, x in enumerate(s1): #создаём словарь для первого слова, где ключ - слово, значения - индексы гласных букв
+#     if x in letters:
+#         dct1.setdefault(s1, []).append(i)
+#
+# for x in s2:
+#     for i, y in enumerate(list(x)): #такой же цикл, но для списка слов
+#         if y in letters:
+#             dct2.setdefault(x, []).append(i)
+#
+# for word, values in dct2.items(): #передаём слова из 2-го словаря
+#     if values == dct1[s1]: #сравниваем значения двух словарей
+#         print(word, end=" ")
+#     else:continue
+# print()
+
+
+#Задача 9-3.Частотный анализ текста
+# s=input("Ввод текста: ")
+#
+# dct = {}
+# for x in s:
+#     dct[x] = dct.get(x, 0) + 1
+#
+# sorted_dct = sorted(dct.items(), key=lambda x: (-x[1], x[0]))
+#
+# i=0
+# for x in sorted_dct:
+#     print(f"{x[0]} - {x[1]}")
+#     i+=1
+#     if i>=10: break
