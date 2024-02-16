9af93f4d7e1df9e3b7c605ba2a28562f3fa872e1
Braynin-Alexandr <braynin.alexander@gmail.com>
2024-02-16 04:32:47 +0300

    дз

---
+++ Home Task/Home Task 14.py
@@ -0,0 +1,51 @@
+# #Задача 14-1
+def count_num(number, count=0):
+    number = str(number)
+    if len(number)>0:
+        return count_num(number[1:], count+1)
+    else:
+        return f'Количество цифр: {count}'
+
+
+number=int(input("Вычислим количество цифр введенного целого числа n (n>=0): "))
+print(count_num(number))
+print()
+
+
+# #Задача 14-2
+class NotNaturalNumberError(Exception):
+    pass
+def check_natural_number(x):
+    if x<=0: raise NotNaturalNumberError(x)
+
+def sum_nat_num(n):
+
+    try:
+        check_natural_number(n)
+
+    except NotNaturalNumberError as e:
+        return f"Ошибка, {e} не является натуральным числом"
+
+    else:
+        if n==1: return 1
+        else:
+            return n + sum_nat_num(n-1)
+
+
+number=int(input("Вычислим сумму цифр натурального числа: "))
+print(sum_nat_num(number))
+print()
+
+
+
+# Задача 14-3
+def tri_2(n):
+    if n==1:
+        print("*")
+        return
+    else:
+        print("*"*n)
+        tri_2(n-1)
+        print("*"* n)
+
+tri_2(int(input("Введите число, чтобы напечатать два треугольника: ")))
