4a1477fb7269d6caa97f6cd6b6d1afd8d33ad638
Braynin-Alexandr <braynin.alexander@gmail.com>
2024-02-21 04:44:07 +0300

    дз

--- Home Task/Home Task 16.py
+++ Home Task/Home Task 16.py
@@ -1,28 +1,31 @@
+# -*- coding: utf-8 -*-
+
 import re
-#Задача 16-1 (1-е решение)
+#Р—Р°РґР°С‡Р° 16-1 (1-Рµ СЂРµС€РµРЅРёРµ)
 def abbreviation(string):
     import re
-    first_letters = re.findall(r"\b(\w)\w*\b", string) #берем из строки только первые буквы
-    return "".join([i.upper() for i in first_letters]) #первые буквы из списка first_letters переводим в верх.регистр и объединяем в одну строку черех join
+    first_letters = re.findall(r"\b(\w)\w*\b", string) #Р±РµСЂРµРј РёР· СЃС‚СЂРѕРєРё С‚РѕР»СЊРєРѕ РїРµСЂРІС‹Рµ Р±СѓРєРІС‹
+    return "".join([i.upper() for i in first_letters]) #РїРµСЂРІС‹Рµ Р±СѓРєРІС‹ РёР· СЃРїРёСЃРєР° first_letters РїРµСЂРµРІРѕРґРёРј РІ РІРµСЂС….СЂРµРіРёСЃС‚СЂ Рё РѕР±СЉРµРґРёРЅСЏРµРј РІ РѕРґРЅСѓ СЃС‚СЂРѕРєСѓ С‡РµСЂРµС… join
 
-text = "Институт точной Механики оптики"
+text = "РРЅСЃС‚РёС‚СѓС‚ С‚РѕС‡РЅРѕР№ РњРµС…Р°РЅРёРєРё РѕРїС‚РёРєРё"
 print(abbreviation(text))
+print()
 
 
-#Задача 16-1 (2-е решение)
+#Р—Р°РґР°С‡Р° 16-1 (2-Рµ СЂРµС€РµРЅРёРµ)
 def abbreviation(string):
     import re
     first_letters = re.sub(r"\b(\w)\w*\b", r"\1", string.upper())
     return "".join(re.split(r'\s+', first_letters))
 
-text = "Институт точной механики оптики"
+text = "РРЅСЃС‚РёС‚СѓС‚ С‚РѕС‡РЅРѕР№ РјРµС…Р°РЅРёРєРё РѕРїС‚РёРєРё"
 print(abbreviation(text))
 print()
 
 
-#Задача 16-2 (1-е решение)
+#Р—Р°РґР°С‡Р° 16-2 (1-Рµ СЂРµС€РµРЅРёРµ)
 string="0, 1, 12, 2,45, 46, 100, 1000, 20000"
-x=int(input())
+x=int(input("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ: "))
 res=[]
 
 for i in range(x+1):
@@ -32,18 +35,20 @@ for i in range(x+1):
 print(", ".join(res))
 print()
 
-#Задача 16-2 (2-е решение)
+
+#Р—Р°РґР°С‡Р° 16-2 (2-Рµ СЂРµС€РµРЅРёРµ)
 
 string="0, 30, -5 1, 12, 2,45, 46, 100, 1000, 20000"
 
-num=int(input())
-pattern= r'\b|\b'.join([str(x) for x in range(-1,num+1)]) #строка из \b 1 \b | \b 2 \b, т.е. "|" это или 1 или 2 и тд; \b окружает числа, чтобы они не были частью другого числа
-pattern+=r'\b'#одной \b не хватает в конеце
-print(pattern)
+num=int(input("Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ: " ))
+pattern= r'\b|\b'.join([str(x) for x in range(-1,num+1)]) #СЃС‚СЂРѕРєР° РёР· \b 1 \b | \b 2 \b, С‚.Рµ. "|" СЌС‚Рѕ РёР»Рё 1 РёР»Рё 2 Рё С‚Рґ; \b РѕРєСЂСѓР¶Р°РµС‚ С‡РёСЃР»Р°, С‡С‚РѕР±С‹ РѕРЅРё РЅРµ Р±С‹Р»Рё С‡Р°СЃС‚СЊСЋ РґСЂСѓРіРѕРіРѕ С‡РёСЃР»Р°
+pattern+=r'\b'#РѕРґРЅРѕР№ \b РЅРµ С…РІР°С‚Р°РµС‚ РІ РєРѕРЅРµС†Рµ
 print(", ".join(re.findall(fr'{pattern}', string)))
 print()
-#Задача 16-3
 
+
+
+#Р—Р°РґР°С‡Р° 16-3
 def decorator(func):
     def wrapper():
         string=func()
