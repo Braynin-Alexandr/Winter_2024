70b105603119a10d6921c4b481b9ebe451c9bdb7
Braynin-Alexandr <braynin.alexander@gmail.com>
2024-03-25 01:34:04 +0300

    2

--- Проект/new.py
+++ Проект/new.py
@@ -4,6 +4,9 @@ from time import time
 from openpyxl import load_workbook, Workbook
 from collections import Counter
 from pprint import pprint
+from docx import Document
+from docx.enum.text import WD_ALIGN_PARAGRAPH
+
 
 def get_test_mark(pupil_answers):
     test_points_marks={6:2, 11:3, 15:4}
@@ -54,16 +57,16 @@ def get_marks_worse(marks):
 
 def get_verification_results(pupil_present, pupil_absent, test_avarage_mark, quarter_3_avarage_mark):
     check_1=round((pupil_absent/ (len(pupil_present)+ pupil_absent))*100)
-    if check_1 > 25: return 'Результат недостоверный: более 25% учеников не явилось на экзамен'
+    if check_1 > 25: return 'результат недостоверный, более 25% учеников не явилось на экзамен'
 
     check_2 = abs(test_avarage_mark-quarter_3_avarage_mark)
-    if check_2>0.5: return 'Результат недостоверный: разница между средней оценкой за тест и средней оценкой за четверть превышает 0.5 баллов'
+    if check_2>0.5: return 'результат недостоверный, разница между средней оценкой за тест и средней оценкой за четверть превышает 0.5 баллов'
 
     pupils_3_mark=0
     for point in pupil_present.values():
         if point[0]==7: pupils_3_mark+=1
     check_3 = round((pupils_3_mark / len(pupil_present)) * 100)
-    if check_3>25: return f'Результат недостоверный: высокий процент учеников на нижней границе оценки'
+    if check_3>25: return f'результат недостоверный, высокий процент учеников на нижней границе оценки'
 
 def get_most_difficult_tasks(tables):
     critical_amount_pupils = round(len(tables)*0.3)
@@ -75,7 +78,7 @@ def get_most_difficult_tasks(tables):
             if answer==0: count_wrong_answers[num_task+1]+=1
 
     most_difficult_tasks=tuple(task for task, number_wrong_answers in count_wrong_answers.items() if number_wrong_answers >= critical_amount_pupils)
-    return most_difficult_tasks
+    return '-' if len(most_difficult_tasks)==0 else most_difficult_tasks
 
 
 info_from_excel = {}
@@ -123,30 +126,64 @@ verification_results=get_verification_results(info_from_excel, pupil_absent, tes
 most_difficult_tasks=get_most_difficult_tasks(pupils_test_answers)
 
 
-# print('Количество учеников класса, которые писали ВПР:', len(info_from_excel))
-# print('Количество учеников класса, которые не писали ВПР:', pupil_absent)
-# print()
-#
-# print('Оценка','3-я четверть','ВПР')
-#
-# for mark in range(5, 1, -1): print(f'{mark}:',count_quarter_3_marks.get(mark,'-'), count_test_marks.get(mark,'-'))
-#
-# else:
-#     print('% качества:', quarter_3_quality, test_quality)
-#     print('% успеваемости:', quarter_3_performance, test_performance)
-#
-# pprint(pupils_test_answers)
-#
-# print('Средний балл по предмету за 3-ю четверть:', quarter_3_avarage_mark)
-# print(f'Средний балл за ВПР: {test_avarage_mark}')
-#
-# print(f'Список участников работы:') #ученик, баллы, оценка
-#
-#
-# print(f'Анализ допущенных ошибок')
-#
-# print(f'Кол-во и % детей, повысивших свою оценку: ')
-# print(f'Кол-во и % детей, понизивших свою оценку: ')
-#
-# print('Проверка достоверности результатов:')
-#
+doc = Document()
+doc.add_heading('Анализ ВПР').alignment = WD_ALIGN_PARAGRAPH.CENTER
+doc.add_paragraph('')
+
+sentences = [
+    'Дата проведения ВПР: ' ,
+    'ФИО учителя: ',
+    f'Класс: ',
+    'Название предмета: ',
+    f'Количество учеников класса, которые писали ВПР: {len(info_from_excel)}.',
+    f'Количество учеников класса, которые не писали ВПР: {pupil_absent}.',
+    f'Средний балл по предмету за 3-ю четверть: {quarter_3_avarage_mark}.',
+    f'Средний балл за ВПР: {test_avarage_mark}.',
+    f'Среднее кол-во решенных заданий: {test_avarage_task_done}.',
+    f'Кол-во и % детей, повысивших свою оценку: {pupil_better_marks[0]}, ({pupil_better_marks[1]}%).',
+    f'Кол-во и % детей, понизивших свою оценку: {pupil_worse_marks[0]}, ({pupil_worse_marks[1]}%).',
+    f'Проверка достоверности результатов: {verification_results}.',
+    f'Задачи, которые вызвали наибольшие трудности: {most_difficult_tasks}.'
+]
+
+for sentence in sentences:
+    doc.add_paragraph(sentence)
+doc.add_paragraph('')
+
+
+table_1 = doc.add_paragraph('Список участников').alignment = WD_ALIGN_PARAGRAPH.CENTER
+table_1 = doc.add_table(rows=len(info_from_excel)+1, cols=3)
+
+headers_table_1 = ['Ученик', 'Баллы ВПР', 'Итоговая оцнка']
+for i, header in enumerate(headers_table_1): table_1.cell(0, i).text = header
+
+for row, (student_id, data) in enumerate(info_from_excel.items(), 1):
+    table_1.cell(row, 0).text = str(student_id)  # ученик
+    table_1.cell(row, 1).text = str(data[0])     # Баллы ВПР
+    table_1.cell(row, 2).text = str(data[1])
+doc.add_paragraph('')
+
+
+table_2 = doc.add_paragraph('Сравнительный анализ основных поазателей').alignment = WD_ALIGN_PARAGRAPH.CENTER
+table_2 = doc.add_table(rows=6, cols=3)
+headers_table_2 = ['Оценка', '3-я четверть', 'ВПР']
+for i, header in enumerate(headers_table_2): table_2.cell(0, i).text = header
+
+marks = [5, 4, 3]
+for i, mark in enumerate(marks, 1):
+    table_2.cell(i, 0).text = str(mark)+':'
+    table_2.cell(i, 1).text = str(count_quarter_3_marks.get(mark, '-'))
+    table_2.cell(i, 2).text = str(count_test_marks.get(mark, '-'))
+
+table_2.cell(4, 0).text = '% качества:'
+table_2.cell(4, 1).text = str(quarter_3_quality)
+table_2.cell(4, 2).text = str(test_quality)
+
+table_2.cell(5, 0).text = '% успеваемости:'
+table_2.cell(5, 1).text = str(quarter_3_performance)
+table_2.cell(5, 2).text = str(test_performance)
+doc.add_paragraph('')
+
+
+# Сохранение документа
+doc.save('тест.docx')
