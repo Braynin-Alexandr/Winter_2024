d8ddad137c4154e18fd27620845d642d84a49780
Braynin-Alexandr <braynin.alexander@gmail.com>
2024-03-27 15:03:55 +0300

    2

--- Home Task/Home Task 29.py
+++ Home Task/Home Task 29.py
@@ -1,49 +1,73 @@
 # -*- coding: utf-8 -*-
 #Задача 29-1
 
+numbers=[int(x) for x in input('Введите цифры через пробел: ').split()]
 
-# numbers=[5,5,2,5]
 #1-й вариант
-# from collections import Counter
-# print(*[num for num, count in Counter(numbers).items() if count==1])
+from collections import Counter
+print(*[num for num, count in Counter(numbers).items() if count==1])
 
-# #2-й вариант
-# print(*[num for num in set(numbers) if numbers.count(num)==1])
+#2-й вариант
+print(*[num for num in set(numbers) if numbers.count(num)==1])
+print()
 
 
-#Задача 29-2
-# def Hamming(str_1,str_2):
-#     return len([i for i in range(len(str_1)) if str_1[i]!=str_2[i]]) #if ( len(str_1)==len(str_2) and (type(str_1)==str and type(str_2)==str) ) else 'Ошибка'
-#
-# print(Hamming('abc','xyz'))
+
+# #Задача 29-2
+def Hamming(str_1,str_2):
+    return len([i for i in range(len(str_1)) if str_1[i]!=str_2[i]])
+
+string_1=input('Введите первую строку: ')
+string_2=input('Введите вторую строку: ')
+print(f'Количество несовпадающих букв: {Hamming(string_1, string_2)}')
+print()
+
 
 
 #Задача 29-3
-# def isomorphic_words(w_1, w_2):
-#
-#     if len(w_1)!=len(word_2): return False
-#
-#     len_w=len(w_1)
-#     map_w = tuple((w_1.index(w_1[i]), w_2.index(w_2[i])) for i in range(len_w))
-#
-#     for point_1, point_2 in map_w:
-#         if point_1!=point_2:
-#             return False
-#     else:
-#         return True
+def isomorphic_words(w_1, w_2):
 
-# True:
-# CBAABC DEFFED
-# XXX YYY
-# RAMBUNCTIOUSLY THERMODYNAMICS
-# False:
-# AB CC
-# XXY XYY
-# ABAB CD
+    if len(w_1)!=len(w_2) or (type(w_1)!=str and type(w_2)!=str): return False
 
-# while True:
-#     word_1= input('Введите 1-е слово: ')
-#     word_2= input('Введите 2-е слово: ')
-#     print('Ответ:', isomorphic_words(word_1,word_2))
-#     print()
+    len_w=len(w_1)
+    map_w = tuple((w_1.index(w_1[i]), w_2.index(w_2[i])) for i in range(len_w))
 
+    for point_1, point_2 in map_w:
+        if point_1!=point_2:
+            return False
+
+    return True
+
+while True:
+    word_1= input('Введите 1-е слово: ')
+    word_2= input('Введите 2-е слово: ')
+    print('Ответ:', isomorphic_words(word_1,word_2))
+    print()
+
+
+#Задача 29-3, 2-й вариант
+def isomorphic_words(w_1, w_2):
+
+    if len(w_1)!=len(w_2) or (type(w_1)!=str and type(w_2)!=str): return False
+
+    pairs_letters_key_w1={}
+    pairs_letters_key_w2={}
+
+    for i in range(len(w_1)):
+        pairs_letters_key_w1.setdefault(w_1[i], set()).add(w_2[i])
+        pairs_letters_key_w2.setdefault(w_2[i], set()).add(w_1[i])
+
+    for letter in pairs_letters_key_w1.values():
+        if len(letter)>1: return False
+
+    for letter in pairs_letters_key_w2.values():
+        if len(letter)>1: return False
+
+    return True
+
+
+while True:
+    word_1=input('Введите 1-е слово: ')
+    word_2=input('Введите 2-е слово: ')
+    print('Ответ:', isomorphic_words(word_1,word_2))
+    print()
